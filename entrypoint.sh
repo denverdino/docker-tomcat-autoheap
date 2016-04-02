923b6f0c55073cb1e0b0f5f116e65985623666b5
Li Yi <denverdino@gmail.com>
2016-04-02 21:54:46 +0800

    Convert tab into space
    Change-Id: I71f3595c710fdd3f791aa1b0bbd17c1bf33f0e30

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,17 @@ limit_in_bytes=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
 # If not default limit_in_bytes in cgroup
 if [ "$limit_in_bytes" -ne "9223372036854771712" ]
 then
-	limit_in_megabytes=$(expr $limit_in_bytes \/ 1048576)
-	heap_size=$(expr $limit_in_megabytes - $RESERVED_MEGABYTES)
+    limit_in_megabytes=$(expr $limit_in_bytes \/ 1048576)
+    heap_size=$(expr $limit_in_megabytes - $RESERVED_MEGABYTES)
     if [ "$heap_size" -gt "$MIN_HEAP_MEGABYTES" ]
     then
-		if [ "$heap_size" -gt "$MAX_HEAP_MEGABYTES" ]
-		then
-			heap_size=$MAX_HEAP_MEGABYTES
-		fi
-		export JAVA_OPTS="-Xmx${heap_size}m $JAVA_OPTS"
-		echo JAVA_OPTS=$JAVA_OPTS
-	fi
+        if [ "$heap_size" -gt "$MAX_HEAP_MEGABYTES" ]
+        then
+            heap_size=$MAX_HEAP_MEGABYTES
+        fi
+        export JAVA_OPTS="-Xmx${heap_size}m $JAVA_OPTS"
+        echo JAVA_OPTS=$JAVA_OPTS
+    fi
 fi
 
 exec catalina.sh run
